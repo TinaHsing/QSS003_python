dc2acedddd7298d531b7f35bcd1cff577277e070
TinaHsing <tina@quantaser.com>
2019-06-18 21:32:25 +0800

    tt

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 
 t1 = time.time()
-for i in xrange (0,10000):
+for i in range (0,10000):
 	GPIO.output(11, True)
 	GPIO.output(11, False)
 t2 = time.time()
