ef9629a4070eb39eac217079ba3163801dcfee26
TinaHsing <tina@quantaser.com>
2019-06-18 23:36:00 +0800

    t

---
+++ test2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>   
+#include <dlfcn.h> 
+#include <stdlib.h>  
+  
+int main(void){   
+   int (*myadd)();//fuction pointer   
+   void *handle;   
+      
+   handle=dlopen("/home/xiang/python/c/libhello.so",RTLD_LAZY);//open lib file   
+    if(!handle)
+    {
+        printf("%s \n",dlerror());
+        exit(1);
+    }
+   myadd=dlsym(handle,"Hello");//call dlsym function   
+      
+   myadd();   
+   dlclose(handle);   
+}
