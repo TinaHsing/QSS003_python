8fb10626d9b82e0052d17a0f2dce4bb36799ed42
TinaHsing <tina@quantaser.com>
2019-06-18 21:50:12 +0800

    ttt

--- test1.py
+++ test1.py
@@ -12,31 +12,31 @@ for i in range (0,100):
 	GPIO.output(11, False)
 t2 = time.time()
 dt = t2-t1
-print(100:)
-print(dt)
+head = "100:"
+print(head+str(dt))
 t1 = time.time()
 for i in range (0,1000):
 	GPIO.output(11, True)
 	GPIO.output(11, False)
 t2 = time.time()
 dt = t2-t1
-print(1000:)
-print(dt)
+head = "100:"
+print(head+str(dt))
 t1 = time.time()
 for i in range (0,10000):
 	GPIO.output(11, True)
 	GPIO.output(11, False)
 t2 = time.time()
 dt = t2-t1
-print(10000:)
-print(dt)
+head = "100:"
+print(head+str(dt))
 t1 = time.time()
 for i in range (0,100000):
 	GPIO.output(11, True)
 	GPIO.output(11, False)
 t2 = time.time()
 dt = t2-t1
-print(100000:)
-print(dt)
+phead = "100:"
+print(head+str(dt))
 
 
