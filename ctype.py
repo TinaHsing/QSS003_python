0ebbaede8c6c79a0e1ee0cae444347766c73b75e
SherryChen18 <earth18@gmail.com>
2019-07-10 19:04:56 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -1,12 +1,12 @@
 from ctypes import *
 
-#m = cdll.LoadLibrary('./hello.so')
-#m.Hello()
+hello = cdll.LoadLibrary('./hello.so')
+hello.Hello()
 
-#m = cdll.LoadLibrary('/home/pi/QSS003_python//led.so')
-#m.led()
+led = cdll.LoadLibrary('/home/pi/QSS003_python//led.so')
+led.led()
 
-LTC1865 = cdll.LoadLibrary('/home/pi/QSS003_python//LTC1865.so')
-LTC1865.LTC_init(0, 0)
-data = LTC1865.LTC_Read(0)
-print(data)
+#LTC1865 = cdll.LoadLibrary('/home/pi/QSS003_python//LTC1865.so')
+#LTC1865.LTC_init(0, 0)
+#data = LTC1865.LTC_Read(0)
+#print(data)
