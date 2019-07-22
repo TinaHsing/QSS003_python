8b86d53b51d10ee722bf80122afafdb2909c4f0a
SherryChen18 <earth18@gmail.com>
2019-07-22 16:21:08 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -14,6 +14,6 @@ print(data)
 C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
 C12880.setup(1,2,3,4)
 data = (c_uint * 288)()
-C12880.readSpectrometer(10,data)
+C12880.readSpectrometer(1,10,data)
 for i in range(0,288):
 	print(data[i])
