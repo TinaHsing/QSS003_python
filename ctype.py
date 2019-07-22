fefbaaa91c8a448b2cf6cf820ea9d76f3a03222f
SherryChen18 <earth18@gmail.com>
2019-07-22 17:32:06 +0800

    add LED

--- ctype.py
+++ ctype.py
@@ -12,8 +12,10 @@ data = ltc.LTC_Read(0)
 print(data)
 
 C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
-C12880.setup(1,2,3,4)
+C12880.setup(1, 2, 3, 4)
 data = (c_uint * 288)()
-C12880.readSpectrometer(1,10,data)
+C12880.readSpectrometer(1, 10, data)
 for i in range(0,288):
 	print(data[i])
+LED_init(1)
+LED_set(1, 25)
