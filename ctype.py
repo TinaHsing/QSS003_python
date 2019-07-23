6712d69b27e88ffac959d7bce82a87c100591d90
SherryChen18 <earth18@gmail.com>
2019-07-23 11:22:51 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -1,9 +1,9 @@
 from ctypes import *
 
 C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
-C12880.LTC_init(0, 0)
-data = C12880.LTC_Read(0)
-print(data)
+#C12880.LTC_init(0, 0)
+#data = C12880.LTC_Read(0)
+#print(data)
 
 C12880.setup(1, 2, 3, 4)
 data = (c_uint * 288)()
