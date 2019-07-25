6663f94c4e7cf40f77fb27aa42f3a841703cb692
SherryChen18 <earth18@gmail.com>
2019-07-25 17:12:37 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -11,7 +11,10 @@ C12880.LED_Set_Current(1, 25) # set LED driver1 current to 25mA
 C12880.LED_Set_Current(2, 15) # set LED driver2 current to 15mA
 C12880.LED_Set_Current(3, 5) # set LED driver3 current to 5mA
 
+string1 = (c_uchar*20)()
+string1 = "Hello World"
+string2 = "1234567890"
 C12880.LCD_Clear()
-C12880.LCD_Write(0, 0, "     Hello World")
-C12880.LCD_Write(6, 1, "1234567890")
+C12880.LCD_Write(0, 0, string1)
+C12880.LCD_Write(6, 1, string2)
 
