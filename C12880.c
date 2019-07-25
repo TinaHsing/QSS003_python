23269df0704af30f70541a12c15938698a9aa312
SherryChen18 <earth18@gmail.com>
2019-07-25 17:48:25 +0800

    Update C12880.c

--- C12880.c
+++ C12880.c
@@ -169,7 +169,7 @@ void LCD_Clear()
 
 void LCD_Write(int x, int y, char *string)
 {
-  print("%s", string);
+  printf("%s", string);
   lcdPosition(g_lcdHandle, x, y);
   lcdPuts(g_lcdHandle, string);
 }
