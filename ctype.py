a01572568999b65ecf179202d395e4e1e0958924
SherryChen18 <earth18@gmail.com>
2019-07-25 14:42:28 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -3,14 +3,12 @@ from ctypes import *
 ledctrl1 = 8
 ledctrl2 = 1
 ledctrl3 = 4
-spec_st = 22
-spec_clk = 23
 
 C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
 #C12880.LTC_Init(0, 0)
 #data = C12880.LTC_Read(0)
 #print(data)
-C12880.Setup(spec_st, spec_clk)
+C12880.Setup()
 
 C12880.LED_Init(ledctrl1)	#init led driver 1
 C12880.LED_Init(ledctrl2)	#init led driver 2   
