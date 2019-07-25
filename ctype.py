59d96600cbf49a73bbf0dbc56325bdbf7f8d093d
SherryChen18 <earth18@gmail.com>
2019-07-25 15:06:07 +0800

    Update ctype.py

--- ctype.py
+++ ctype.py
@@ -1,29 +1,18 @@
 from ctypes import *
 
-ledctrl1 = 8
-ledctrl2 = 1
-ledctrl3 = 4
-
 C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
-#C12880.LTC_Init(0, 0)
-#data = C12880.LTC_Read(0)
-#print(data)
 C12880.Setup()
 
-C12880.LED_Init(ledctrl1)	#init led driver 1
-C12880.LED_Init(ledctrl2)	#init led driver 2   
-C12880.LED_Init(ledctrl3)	#init led driver 3
-C12880.LED_Set(ledctrl1, 25) # set LED driver1 current to 25mA
-C12880.LED_Set(ledctrl2, 10) # set LED driver2 current to 10mA
-C12880.LED_Set(ledctrl3, 4) # set LED driver3 current to 4mA
+C12880.LED_Set_Current(1, 25) # set LED driver1 current to 25mA
+C12880.LED_Set_Current(2, 15) # set LED driver2 current to 15mA
+C12880.LED_Set_Current(3, 5) # set LED driver3 current to 5mA
 
 data = (c_uint * 288)()
 
-C12880.ReadSpectrometer(1, 10, data)
+C12880.ReadSpectrometer(10, data)
 for i in range(0,288):
 	print(data[i])
 
-C12880.LCD_Init()
 C12880.LCD_Clear()
 C12880.LCD_Write(0, 0, "     Hello World")
 C12880.LCD_Write(6, 1, "1234567890")
