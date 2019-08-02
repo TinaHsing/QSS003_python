e6a22a1465919afd43027629871eb457115681bc
TinaHsing <tina@quantaser.com>
2019-08-02 19:21:26 +0800

    t

--- ctype.py
+++ ctype.py
@@ -5,13 +5,13 @@ C12880 = cdll.LoadLibrary('/home/pi/QSS003_python//C12880.so')
 C12880.Setup()
 time.sleep(1)
 data = (c_uint * 288)()
-C12880.ReadSpectrometer(10, data)
+#C12880.ReadSpectrometer(10, data)
 #for i in range(0,288):
 #	print(data[i])
 
-C12880.LED_Set_Current(1, 25) # set LED driver1 current to 25mA
-C12880.LED_Set_Current(2, 15) # set LED driver2 current to 15mA
-C12880.LED_Set_Current(3, 5) # set LED driver3 current to 5mA
+#C12880.LED_Set_Current(1, 25) # set LED driver1 current to 25mA
+#C12880.LED_Set_Current(2, 15) # set LED driver2 current to 15mA
+#C12880.LED_Set_Current(3, 5) # set LED driver3 current to 5mA
 
 for i in range(0,10):
 	print(i)
