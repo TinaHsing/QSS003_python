ae17a64cd7b17e52c3c8b9075be1feeaf394cf86
SherryChen18 <earth18@gmail.com>
2019-08-26 12:36:22 +0800

    Update ShowIP.py

--- ShowIP.py
+++ ShowIP.py
@@ -7,19 +7,21 @@ HOME_DIR = "/home/pi/QSS003_python/"
 IP_FILENAME = HOME_DIR + "IP.txt"
 
 ip = subprocess.check_output(["hostname","-I"])
+ip = str(ip)	# only for python3
 
 fp = open(IP_FILENAME, "w+")
-while (ip == "\n"):
+while (len(ip) < 5):
 	fp.writelines("NO IP\n")
 	time.sleep(1)
 	ip = subprocess.check_output(["hostname","-I"])
+	ip = str(ip)	# only for python3
 
-#ip = str(ip)	# only for python3
 print(ip)
 fp.writelines(ip)
+fp.writelines("\n")
 fp.close()
 
-#ip = ip[2:-4]	# only for python3
+ip = ip[2:-4]	# only for python3
 #print(ip)
 lcd.clear()
 lcd.cursor_pos = (0, 0)
