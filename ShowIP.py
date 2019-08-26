c0fc62615ef6fb53debe5d1f6c61ed05248b59df
SherryChen18 <earth18@gmail.com>
2019-08-26 12:50:43 +0800

    Update ShowIP.py

--- ShowIP.py
+++ ShowIP.py
@@ -3,23 +3,23 @@ import subprocess
 from RPLCD.i2c import CharLCD
 
 lcd = CharLCD('PCF8574', address=0x27, port=1, backlight_enabled=True)
-HOME_DIR = "/home/pi/QSS003_python/"
-IP_FILENAME = HOME_DIR + "IP.txt"
+#HOME_DIR = "/home/pi/QSS003_python/"
+#IP_FILENAME = HOME_DIR + "IP.txt"
 
 ip = subprocess.check_output(["hostname","-I"])
 ip = str(ip)	# only for python3
 
-fp = open(IP_FILENAME, "w+")
+#fp = open(IP_FILENAME, "w+")
 while (len(ip) < 9):
-	fp.writelines("NO IP\n")
+#	fp.writelines("NO IP\n")
 	time.sleep(1)
 	ip = subprocess.check_output(["hostname","-I"])
 	ip = str(ip)	# only for python3
 
-print(ip)
-fp.writelines(ip)
-fp.writelines("\n")
-fp.close()
+#print(ip)
+#fp.writelines(ip)
+#fp.writelines("\n")
+#fp.close()
 
 ip = ip[2:-4]	# only for python3
 #print(ip)
