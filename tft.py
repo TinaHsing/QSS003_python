d6d451ef5393e10cc086d0a43883c071e1026e51
SherryChen18 <sherry@quantaser.com>
2019-10-08 18:02:47 +0800

    succuss use spi1

--- tft.py
+++ tft.py
@@ -5,14 +5,15 @@ import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import ST7735 as TFT
 
-spi = SPI.SpiDev(1, 0, max_speed_hz = 1000000)
+spi = SPI.SpiDev(1, 0, max_speed_hz = 4000000)
 
-disp =TFT.ST7735(dc=2, rst = 0, spi = spi, width = 128, height =128)
+disp =TFT.ST7735(dc=27, rst = 17, spi = spi, width = 128, height =128)
 disp.begin()
+disp.clear()
 
-bk = Image.new('RGB', (128,128), (255,0,0))
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",30)
+bk = Image.new('RGB', (128,128), (255,255,255))
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",14)
 draw = ImageDraw.Draw(bk)
-draw.text((10,10),"Hello", font=font, fill =(0,255,0))
+draw.text((10,10),"Hello", font=font, fill =(0,0,0))
 
 disp.display(bk)
