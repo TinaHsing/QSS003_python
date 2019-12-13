a79ac9614b800be1cd409e362f01949203d501b7
SherryChen18 <earth18@gmail.com>
2019-12-13 16:57:07 +0800

    Update pwm_test.py

--- pwm_test.py
+++ pwm_test.py
@@ -1,20 +1,30 @@
 import pigpio
 import RPi.GPIO as GPIO
+import time
 
-GATE_PIN = 16
+GATE_PIN = 15
 PWM_LED_PIN = 18
 PWM_LED_PIN2 = 13
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(GATE_PIN, GPIO.OUT)
 
-GPIO.output(GATE_PIN, GPIO.HIGH)
-#GPIO.output(GATE_PIN, GPIO.LOW)
+GPIO.output(GATE_PIN, GPIO.HIGH)	#close
+#GPIO.output(GATE_PIN, GPIO.LOW)	#open
 
 PWM_FREQ = 500
-DUTY = 500000
-DUTY2 = 200000 
+DUTY = 100000	#0~1000000
+DUTY2 = 100000
+
 pi = pigpio.pi()
-pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, DUTY)
+#pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, DUTY)
 pi.hardware_PWM(PWM_LED_PIN2, PWM_FREQ, DUTY2)
 
+#
+for i in range(0,11):
+        pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, DUTY*i)
+        GPIO.output(GATE_PIN, GPIO.LOW)
+        time.sleep(10)
+        GPIO.output(GATE_PIN, GPIO.HIGH)
+        time.sleep(5)
+##
